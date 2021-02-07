7c4945009b756b1a390decc5ce8d0484b2d01481
aki <aki.akaguma@hotmail.com>
2021-02-07 23:21:18 +0900

    version 0.1.10

---
+++ src/run.rs
@@ -0,0 +1,21 @@
+use crate::conf::CmdOptConf;
+use crate::util::adapt_input;
+use crate::util::err::BrokenPipeError;
+use runnel::StreamIoe;
+
+pub fn run(sioe: &StreamIoe, conf: &CmdOptConf) -> anyhow::Result<()> {
+    //println!("{:?}", conf);
+    //
+    let r = run_0(sioe, &conf.arg_params);
+    if r.is_broken_pipe() {
+        return Ok(());
+    }
+    r
+}
+
+fn run_0(sioe: &StreamIoe, files: &[String]) -> anyhow::Result<()> {
+    adapt_input(sioe, files, |reader| -> anyhow::Result<()> {
+        std::io::copy(reader, &mut sioe.sout.lock())?;
+        Ok(())
+    })
+}
