9a94f132c8469b32852076f7df563c0e70d179dc
aki <aki.akaguma@hotmail.com>
2021-04-24 08:25:19 +0900

    version 0.1.22

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,8 @@
-pub use self::adapt_input::adapt_input;
-pub mod adapt_input;
 pub mod err;
+
+mod opt_uc_x_param;
+pub use self::opt_uc_x_param::OptUcXParam;
+pub use self::opt_uc_x_param::OptUcXParamParseError;
+
+pub mod adapt_input;
+pub use self::adapt_input::adapt_input;
